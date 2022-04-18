4fe6ae213cc6ae3dc2146e106bc4b1806e73c792
albright.human <sungutialbright@gmail.com>
2022-04-18 15:40:17 +0300

    add github URL to the service

---
+++ src/app/api.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { Repo } from './repo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  username: string = 'sunguti-albright';
+
+  GIT_API_URL: string = 'https://api.github.com/users/';
+  constructor(private http: HttpClient) {}
+
+//   SearchGithubUsers(): Observable<User> {
+//     return this.http.get<User>(this.GIT_API_URL + this.username);
+//   }
+//   // getUsers(): Observable<User> {
+//   //   return this.http.get<User>(this.GIT_API_URL + this.username);
+//   // }
+
+//   getPublicRepos(): Observable<Repo> {
+//     return this.http.get<Repo>(this.GIT_API_URL + this.username + '/repos');
+//   }
+//   UpdateUser(githubUser: string) {
+//     this.username = githubUser;
+//   }
+// }
