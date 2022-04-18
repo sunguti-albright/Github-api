06b766179ff5ee4863c8154f539c71ea48e5ce78
albright.human <sungutialbright@gmail.com>
2022-04-18 15:43:42 +0300

    create class for user data

---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+   public id: number,
+   public  name: string,
+   public username: string,
+   public  login: string,
+   public  bio: string,
+   public  followers:number,
+   public  following:number,
+   public  public_repos:number,
+   public  avatar_url:string,
+   public  html_url:string
+    ){}
+}
